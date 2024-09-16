56f0c498b049673ef5534711f2add9538f1bee0b
krasi <krasimir.xristov@gmail.com>
2024-09-16 20:22:02 +0200

    Test code to add a Buy button in the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@nextui-org/button';
 import Image from 'next/image';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button color='primary'>Buy</Button>
+    </div>
+  );
 };
 
 export default Home;
