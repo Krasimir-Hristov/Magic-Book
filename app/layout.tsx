8301fc082b089afe26627417f19cc3f0c9b30725
krasi <krasimir.xristov@gmail.com>
2024-09-17 21:33:49 +0200

    Refactor layout.tsx to add font weight 700 to PoppinsFont

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const FredokaFont = Fredoka({
 
 const PoppinsFont = Poppins({
   subsets: ['latin'],
-  weight: ['400', '500', '600'],
+  weight: ['400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
