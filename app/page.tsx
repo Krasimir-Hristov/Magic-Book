51824927feb96022ea3405df0744d5ba08f6d82c
krasi <krasimir.xristov@gmail.com>
2024-09-18 20:05:15 +0200

    Refactor layout.tsx to remove Buy button from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,7 @@ import { Button } from '@nextui-org/button';
 import Image from 'next/image';
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Button color='primary'>Buy</Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
