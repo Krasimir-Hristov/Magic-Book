65fd57b202fea53c98c10222744123cee4f4b9fb
krasi <krasimir.xristov@gmail.com>
2024-09-18 20:15:02 +0200

    Refactor Header component to add logo and brand name

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -9,9 +9,20 @@ import {
   NavbarMenu,
   NavbarMenuItem,
 } from '@nextui-org/navbar';
+import Image from 'next/image';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar>
+      <NavbarContent>
+        <NavbarBrand>
+          <Image src={'/logo.svg'} alt='Logo' width={40} height={40} />
+          <h2 className='font-bold text-2xl text-primary ml-3'>Magic Book</h2>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent></NavbarContent>
+    </Navbar>
+  );
 };
 
 export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { Button } from '@nextui-org/button';
 import Image from 'next/image';
+import Header from './_components/Header';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
