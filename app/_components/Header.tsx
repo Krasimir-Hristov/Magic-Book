62cae7307680adfe1b9c6efcec6c4b48f16f2b0d
krasi <krasimir.xristov@gmail.com>
2024-09-19 21:06:24 +0200

    Refactor Header component to add menu items and responsive menu

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 
 import {
   Navbar,
@@ -21,22 +23,33 @@ const Header = () => {
     { name: 'Explore Books', path: '/explore' },
     { name: 'Contact Us', path: '/contact-us' },
   ];
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <Navbar maxWidth='full' className='bg-[#E0F7FA]'>
+    <Navbar
+      maxWidth='full'
+      className='bg-[#E0F7FA] justify-between'
+      onMenuOpenChange={setIsMenuOpen}
+    >
       <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close Menu' : 'Open Menu'}
+          className='sm:hidden text-blue-700'
+        />
         <NavbarBrand>
           <Image
             src={'/logo.svg'}
             alt='Logo'
             width={40}
             height={40}
-            className='text-[#FF7043]'
+            className='text-[#FF7043] '
           />
 
           <h2 className='font-bold text-2xl text-blue-700 ml-3'>Magic Book</h2>
         </NavbarBrand>
       </NavbarContent>
-      <NavbarContent justify='center'>
+      <NavbarContent justify='center' className='hidden sm:flex'>
         {menuList.map((item, index) => (
           <NavbarItem className='text-xl text-primary font-medium mx-2 hover:underline'>
             <Link className='text-[#1A237E]' href={item.path}>
@@ -51,6 +64,16 @@ const Header = () => {
           Get Started
         </Button>
       </NavbarContent>
+
+      <NavbarMenu>
+        {menuList.map((item, index) => (
+          <NavbarMenuItem>
+            <Link href={item.path} className='text-[#FF7043]'>
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 };
