5fd64c65091ace634266d4efcb36ca917dfcfc94
krasi <krasimir.xristov@gmail.com>
2024-09-26 20:11:17 +0200

    style the signIn page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,22 @@
 import { SignIn } from '@clerk/nextjs';
+import Image from 'next/image';
 
 export default function Page() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 bg-[#80adaf]'>
-      <div className='flex justify-center items-center h-screen'>
+    <div className='grid grid-cols-1 md:grid-cols-2 bg-[#6c7878] min-h-screen'>
+      {/* Image container */}
+      <div className='flex justify-center items-center p-4 md:p-8'>
+        <Image
+          src={'/mblogin.png'}
+          alt='login'
+          width={700}
+          height={1000}
+          className='w-full h-auto max-w-xs md:max-w-full'
+        />
+      </div>
+
+      {/* SignIn form container */}
+      <div className='flex flex-col justify-center items-center p-4 md:p-8'>
         <SignIn />
       </div>
     </div>
