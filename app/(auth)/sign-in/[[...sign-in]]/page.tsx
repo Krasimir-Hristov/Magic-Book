888c73346e0b81eca4ba2cf9309e6e49eeaadd91
krasi <krasimir.xristov@gmail.com>
2024-09-26 19:48:27 +0200

    change the background in the signin page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 bg-[#80adaf]'>
+      <div className='flex justify-center items-center h-screen'>
+        <SignIn />
+      </div>
+    </div>
+  );
 }
