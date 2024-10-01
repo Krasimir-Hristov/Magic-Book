a5dff278ad6486978cafebbec8ef9cbaa9f527eb
krasi <krasimir.xristov@gmail.com>
2024-10-01 14:38:14 +0200

    finish the Book type component

--- app/create-book/_components/BookType.tsx
+++ app/create-book/_components/BookType.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 const BookType = () => {
   const optionList = [
@@ -20,12 +22,22 @@ const BookType = () => {
     },
   ];
 
+  const [selectedOption, setSelectedOption] = useState<string>('');
+
   return (
     <div>
       <label className='font-bold text-4xl text-[#1A237E]'>2. Book Type</label>
       <div className='grid grid-cols-3 gap-5 mt-3 '>
         {optionList.map((item, index) => (
-          <div className='relative grayscale hover:grayscale-0 cursor-pointer'>
+          <div
+            className={`relative grayscale hover:grayscale-0 cursor-pointer p-1
+               ${
+                 selectedOption == item.label
+                   ? 'grayscale-0 border-2 rounded-3xl border-[#1A237E]'
+                   : 'grayscale'
+               }`}
+            onClick={() => setSelectedOption(item.label)}
+          >
             <h2 className='absolute bottom-5 text-white text-xl font-bold text-center w-full'>
               {item.label}
             </h2>
@@ -33,7 +45,7 @@ const BookType = () => {
               src={item.imageUrl}
               alt={item.label}
               width={300}
-              height={400}
+              height={500}
               className='object-cover h-[260px] rounded-3xl'
             />
           </div>
