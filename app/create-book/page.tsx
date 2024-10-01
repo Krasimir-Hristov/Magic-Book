14f8a19d01d0e86da81d9898636efc8087c4d700
krasi <krasimir.xristov@gmail.com>
2024-10-01 10:27:14 +0200

    add some comments in the create book page

--- app/create-book/page.tsx
+++ app/create-book/page.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const CreateBook = () => {
-  return <div className='bg-[#80adaf]'>CreateBook</div>;
+  return (
+    <div className='bg-[#80adaf] p-10 md:px-20 lg:px-40'>
+      <h2 className='font-extrabold  text-[70px] text-[#1A237E] text-center'>
+        CREATE YOUR BOOK
+      </h2>
+      <p className='text-2xl text-[#1A237E] text-center'>
+        "Unlock your creativity with AI: Create stories like never before! Let
+        our AI bring your stories to life, one by one.
+      </p>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-14'>
+        {/* Book Subject */}
+
+        {/* Book Type */}
+
+        {/* Age Group */}
+
+        {/* Image Style */}
+      </div>
+    </div>
+  );
 };
 
 export default CreateBook;
