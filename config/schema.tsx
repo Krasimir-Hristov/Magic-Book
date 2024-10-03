4ed4ad757ad279bb2dc441d354248c065f4b9adf
krasi <krasimir.xristov@gmail.com>
2024-10-03 13:12:02 +0200

    setupt my db

---
+++ config/schema.tsx
@@ -0,0 +1,12 @@
+import { json, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+
+export const BookData = pgTable('storyData', {
+  id: serial('id').primaryKey(),
+  stpryId: varchar('storyId'),
+  storySubject: text('storySubject'),
+  storyType: varchar('storyType'),
+  ageGroup: varchar('ageGroup'),
+  imageStype: varchar('imageStyle'),
+  output: json('output'),
+  coverImage: varchar('coverImage'),
+});
